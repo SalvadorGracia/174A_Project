1a9e214ac974d0cc0543aac09c6c063910fdec6f
SalvadorGracia <113317058+SalvadorGracia@users.noreply.github.com>
2025-05-27 22:02:08 -0700

    Update ivc.sql - Course_Offering

--- ivc.sql
+++ ivc.sql
@@ -1,86 +1,75 @@
 CREATE TABLE Student_Is_In (
-	perm_number CHAR(7),
-    sname CHAR(20),
-    address CHAR(20),
-    PIN CHAR(4),
-	mname CHAR(20),
-	dname CHAR(20),
-	PRIMARY KEY (perm_number),
-	FOREIGN KEY (mname) REFERENCES Major,
-	FOREIGN KEY (dname) REFERENCES Department
+	perm_number CHAR(7) PRIMARY KEY,
+    	s_name CHAR(250),
+    	address CHAR(250),
+    	pin CHAR(4),
+	m_name CHAR(250),
+	d_name CHAR(250),
+	FOREIGN KEY (m_name) REFERENCES Major,
+	FOREIGN KEY (d_name) REFERENCES Department
 );
-CREATE TABLE Courses (
+CREATE TABLE Course_Offering (
+	enrollment_code INTEGER PRIMARY KEY,
 	course_number CHAR(7),
-	enrollment_code INTEGER,
-	title CHAR(20),
-	pname CHAR(15),
-	location CHAR(9),
-	time_slot CHAR(20),
+	location CHAR(250),
+	title CHAR(250),
 	max_enrollment INTEGER,
-	PRIMARY KEY (course_number),
-	UNIQUE (enrollment_code)
-);
-CREATE TABLE Quarter_Year (
-	quarter CHAR(6),
+	p_name CHAR(250),
+	quarter CHAR(250),
 	year INTEGER,
-	PRIMARY KEY (quarter)
+	time_slot CHAR(250),
+	FOREIGN KEY (course_number) REFERENCES Course_Catalog
+);
+CREATE TABLE Course_Catalog (
+	course_number CHAR(7) PRIMARY KEY
 );
 CREATE TABLE Department (
-	dname CHAR(20),
-	PRIMARY KEY(dname)
+	d_name CHAR(250) PRIMARY KEY
 );
 CREATE TABLE Major (
-	mname CHAR(20),
-	elective_number INTEGER,
-	PRIMARY KEY (mname)
+	m_name CHAR(250) PRIMARY KEY,
+	elective_number INTEGER
 );
 CREATE TABLE Current_Courses (
 	perm_number CHAR(7),
-	course_number CHAR(7),
-	PRIMARY KEY (perm_number, course_number),
+	enrollment_code INTEGER,
+	PRIMARY KEY (perm_number, enrollment_code),
 	FOREIGN KEY (perm_number) REFERENCES Student_Is_In,
-	FOREIGN KEY (course_number) REFERENCES Courses	
+	FOREIGN KEY (enrollment_code) REFERENCES Course_Offering	
 );
 CREATE TABLE Past_Courses (
 	perm_number CHAR(7),
-	course_number CHAR(7),
+	enrollment_code CHAR(7),
 	grade CHAR(2),
-	PRIMARY KEY (perm_number, course_number),
+	PRIMARY KEY (perm_number, enrollment_code),
 	FOREIGN KEY (perm_number) REFERENCES Student_Is_In,
-	FOREIGN KEY (course_number) REFERENCES Courses	
+	FOREIGN KEY (enrollment_code) REFERENCES Course_Offering	
 );
 CREATE TABLE Prerequisites (
 	course_number CHAR(7),
 	prereq_course CHAR(7),
 	PRIMARY KEY (course_number, prereq_course),
-	FOREIGN KEY (course_number) References Courses,
-	FOREIGN KEY (prereq_course) References Courses(course_number)
-);
-CREATE TABLE Offered (
-	course_number CHAR(7),
-	quarter CHAR(6),
-	PRIMARY KEY (course_number, quarter),
-	FOREIGN KEY (course_number) REFERENCES Courses,
-	FOREIGN KEY (quarter) REFERENCES Quarter_Year
+	FOREIGN KEY (course_number) References Course_Catalog,
+	FOREIGN KEY (prereq_course) References Course_Catalog(course_number)
 );
 CREATE TABLE Mandatory_Courses (
-	mname CHAR(20),
+	m_name CHAR(250),
 	course_number CHAR(7),
-	PRIMARY KEY (mname, course_number),
-	FOREIGN KEY (mname) REFERENCES Major,
-	FOREIGN KEY (course_number) REFERENCES Courses
+	PRIMARY KEY (m_name, course_number),
+	FOREIGN KEY (m_name) REFERENCES Major,
+	FOREIGN KEY (course_number) REFERENCES Course_Catalog
 );
-CREATE TABLE Major_Electives (
-	mname CHAR(20),
+CREATE TABLE Elective_Courses (
+	m_name CHAR(250),
 	course_number CHAR(7),
-	PRIMARY KEY (mname, course_number),
-	FOREIGN KEY (mname) REFERENCES Major,
-	FOREIGN KEY (course_number) REFERENCES Courses
+	PRIMARY KEY (m_name, course_number),
+	FOREIGN KEY (m_name) REFERENCES Major,
+	FOREIGN KEY (course_number) REFERENCES Course_Catalog
 );
 CREATE TABLE Manages (
-	dname CHAR(20),
-	mname CHAR(20),
-	PRIMARY KEY (dname, mname),
-	FOREIGN KEY (dname) REFERENCES Department,
-	FOREIGN KEY (mname) REFERENCES Major
+	d_name CHAR(250),
+	m_name CHAR(250),
+	PRIMARY KEY (d_name, m_name),
+	FOREIGN KEY (d_name) REFERENCES Department,
+	FOREIGN KEY (m_name) REFERENCES Major
 );
