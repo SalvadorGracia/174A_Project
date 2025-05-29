b61c64e3d3e7745d37627a33ae2bf3caf00bcef5
SalvadorGracia <salvadorgracia@ucsb.edu>
2025-05-29 16:29:44 -0700

    Uploaded Previous Data

--- src/main/java/edu/ucsb174a/App.java
+++ src/main/java/edu/ucsb174a/App.java
@@ -1,10 +1,13 @@
 package edu.ucsb174a;
 
-/**
- * Hello world!
- */
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Dotenv dotenv = Dotenv.configure().load();
+        String db_url = dotenv.get("DB_URL");
+        String db_user = dotenv.get("DB_USER");
+        String db_password = dotenv.get("DB_PASSWORD");
+        System.out.println(db_url + "\n" + db_user + "\n" + db_password);
     }
 }
